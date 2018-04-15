f781c9815871d327b7b69a8d5ff75c902f20cf63
Naveen Kumar <37360520+Naveenkrs@users.noreply.github.com>
2018-04-15 19:58:18 +0530

    Add files via upload

---
+++ Player Swip Movement/Swip.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swip : MonoBehaviour 
+
+    {
+	private bool tap,swipLeft,swipRight,swipUp,swipDown;
+	private bool isDraging = false;
+	private Vector3 startTouch, swipDelta;
+
+
+	private void Update()
+	{
+		tap = swipLeft = swipRight = swipUp = swipDown = false;
+
+		#region Standalone Inputs
+		if(Input.GetMouseButtonDown(0))
+		{
+			isDraging = true;
+			tap = true;
+			startTouch = Input.mousePosition;
+		}
+		else if(Input.GetMouseButtonUp(0))
+		{
+			isDraging = false;
+			Reset();
+		
+		}
+		#endregion
+
+#region Mobile Inputs
+
+		if(Input.touches.Length > 0)
+		{
+			if(Input.touches[0].phase == TouchPhase.Began)
+			{
+				tap= true;
+				isDraging = true;
+				startTouch = Input.touches[0].position;
+			}
+			else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+			{
+				isDraging = false;
+				Reset();
+			}
+		}
+
+#endregion
+
+		//calculate the distance 
+
+		swipDelta = Vector3.zero;
+		if(isDraging)
+		{
+			if(Input.touches.Length >0)
+			
+				swipDelta = (Vector3)Input.touches[0].position - startTouch;
+				else if(Input.GetMouseButton(0))
+				swipDelta = (Vector3)Input.mousePosition - startTouch;
+		}
+
+		// Dis we Cross The deadZone
+		if(swipDelta.magnitude > 125)
+		{
+			//Which Direction 
+			float x = swipDelta.x;
+			float y = swipDelta.y;
+			if(Mathf.Abs(x) > Mathf.Abs(y))
+			{
+				//Left or Right
+				if(x < 0)
+					swipLeft = true;
+				else
+					swipRight = true;
+			}
+			else
+			{
+				// up or Down1
+				if(x < 0)
+					swipDown = true;
+				else
+					swipUp = true;
+			}
+			Reset();
+		}
+
+	}
+	private void Reset()
+	{
+		startTouch = swipDelta = Vector3.zero;
+		isDraging = false;
+	}
+	public Vector3 SwipDelta{get{return swipDelta;}}
+	public bool SwipLeft{get{return swipLeft;}}
+	public bool SwipRight{get{return swipRight;}}
+	public bool SwipUp{get{return swipUp;}}
+	public bool SwipDown{get{return swipDown;}}
+
+}
+
---
+++ Player Swip Movement/Swipp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swipp : MonoBehaviour {
+
+	public Swip swipeControls;
+	public Transform Player;
+	private Vector3 desiredPosition;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (swipeControls.SwipLeft)
+			desiredPosition += Vector3.left;
+		if (swipeControls.SwipRight)
+			desiredPosition += Vector3.right;
+		if (swipeControls.SwipUp)
+			desiredPosition += Vector3.forward;
+		if (swipeControls.SwipDown)
+			desiredPosition += Vector3.back;
+	
+Player.transform.position = Vector3.MoveTowards (Player.transform.position, desiredPosition, 3f * Time.deltaTime);
+
+	}
+}
